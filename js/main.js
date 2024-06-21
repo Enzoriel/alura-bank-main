14c46bb824c9bf1f2242c4a5526ee408f2a857fe
Enzoriel <enzoknsb@outlook.com>
2024-06-21 13:40:46 -0300

    Validando cuil

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+import esUnCuil from "./validarCuil.js";
+
 const camposFormmulario = document.querySelectorAll("[required]");
 
-camposFormmulario.forEach((campo) => {});
+camposFormmulario.forEach((campo) => {
+  campo.addEventListener("blur", () => verificarCampo(campo));
+});
+
+function verificarCampo(campo) {
+  if (campo.name == "cuil" && campo.value.length >= 11) {
+    esUnCuil(campo);
+  }
+}
