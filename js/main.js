5c82d9917b38b7b3de53afbc43b44cc2ce5d6aba
Enzoriel <enzoknsb@outlook.com>
2024-06-22 12:47:41 -0300

    Validando cuil y edad

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 import esUnCuil from "./validarCuil.js";
+import esMayorEdad from "./validarEdad.js";
 
-const camposFormmulario = document.querySelectorAll("[required]");
+const camposFormulario = document.querySelectorAll("[required]");
 
-camposFormmulario.forEach((campo) => {
+camposFormulario.forEach((campo) => {
   campo.addEventListener("blur", () => verificarCampo(campo));
 });
 
 function verificarCampo(campo) {
   if (campo.name == "cuil" && campo.value.length >= 11) {
     esUnCuil(campo);
+  }
+  if (campo.name == "fecha_nacimiento" && campo.value != "") {
+    esMayorEdad(campo);
   }
 }
--- js/validarCuil.js
+++ js/validarCuil.js
@@ -2,8 +2,13 @@ export default function esUnCuil(campo) {
   const cuil = campo.value.replace(/[-\/]/g, "");
   tieneNumRepetidos(cuil);
 
-  console.log(cuil);
-  console.log(tieneNumRepetidos(cuil));
+  if (tieneNumRepetidos(cuil)) {
+    console.log("Hay numeros repetidos, no te pases de verga");
+  } else if (validarDigitoVerificador(cuil) && validarPrimDigitos(cuil)) {
+    console.log("Cuil válido, crack, pasá que te esperamos en la sala VIP");
+  } else {
+    console.log("Este cuil no existe, te quisiste hacer el gracioso y sos tremendo pelotudo");
+  }
 }
 
 function tieneNumRepetidos(cuil) {
@@ -21,4 +26,35 @@ function tieneNumRepetidos(cuil) {
   ];
 
   return numRepetidos.includes(cuil);
+}
+
+function validarPrimDigitos(cuil) {
+  let primerosDigitos = cuil.substr(0, 2);
+  const digitosValidos = ["20", "23", "24", "27", "30", "33", "34"];
+
+  return digitosValidos.includes(primerosDigitos);
+}
+
+function validarDigitoVerificador(cuil) {
+  const factores = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+  let acumulado = 0;
+
+  for (let i = 0; i < 10; i++) {
+    acumulado += parseInt(cuil[i], 10) * factores[i];
+  }
+
+  console.log(`Este es el acumulado: ${acumulado}`);
+  let verificadorTeorico = 11 - (acumulado % 11);
+  console.log(`Este es el verificador teórico ${verificadorTeorico}`);
+
+  if (verificadorTeorico === 10) {
+    verificadorTeorico = 9;
+  } else if (verificadorTeorico === 11) {
+    verificadorTeorico = 0;
+  }
+
+  console.log(`Este es el resultado del verificador teorico ${verificadorTeorico}`);
+
+  const digitoVerificador = parseInt(cuil[10], 10);
+  return digitoVerificador === verificadorTeorico;
 }
